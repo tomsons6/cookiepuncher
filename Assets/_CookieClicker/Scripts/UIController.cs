d45abc97da95edd6e587c92f4cb06a5c4ebb55a6
tomsons6 <toms.amsons@va.lv>
2024-01-03 01:07:57 +0200

    main menu update
    added info text

--- Assets/_CookieClicker/Scripts/UIController.cs
+++ Assets/_CookieClicker/Scripts/UIController.cs
@@ -7,6 +7,7 @@ public class UIController : MonoBehaviour
 {
     public TMP_Text cookieCount;
     public TMP_Text strenght;
+    public List<GameObject> panels;
 
     private void Start()
     {
@@ -17,5 +18,20 @@ public class UIController : MonoBehaviour
     {
         cookieCount.text = "Coockies crunched - " + string.Format("{0:0.0}", GameManager.Instance.playerStats.cookieCount);
         strenght.text = "Current strenght - " + string.Format("{0:0.0}", GameManager.Instance.playerStats.punchStrenght);
+    }
+    public void SwitchPanel(GameObject panelToOpen)
+    {
+        foreach (GameObject panel in panels)
+        {
+            if(panel != panelToOpen)
+            {
+                panel.SetActive(false);
+            }
+            else
+            {
+                panel.SetActive(true);
+            }
+
+        }
     }
 }
