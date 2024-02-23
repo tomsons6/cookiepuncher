69a226fd05ad5379a35cb14a12128b28fa51a6a2
tomsons6 <toms.amsons@va.lv>
2024-02-24 01:13:21 +0200

    start sequence texts adjusted and loco instruction

--- Assets/_CookieClicker/Scripts/TutorialSequence.cs
+++ Assets/_CookieClicker/Scripts/TutorialSequence.cs
@@ -1,3 +1,4 @@
+using BNG;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -12,14 +13,17 @@ public class TutorialSequence : MonoBehaviour
     enum startSequence { started, showInstruction, enableMarker, finished }
     startSequence sequence;
     bool markerAnimationActive = false;
+    SmoothLocomotion locomotion;
 
     const float startHeight = 0;
+    private bool areInfoTextRunning = false;
     // Start is called before the first frame update
     void Start()
     {
         if (GameManager.Instance.playerStats.IntroSequence == false)
         {
             StartCoroutine(StartSequence());
+            locomotion = rigCanva.GetComponentInParent<SmoothLocomotion>();
         }
         else
         {
@@ -41,8 +45,13 @@ public class TutorialSequence : MonoBehaviour
                     sequence = startSequence.showInstruction;
                     break;
                 case startSequence.showInstruction:
-                    rigCanva.GetComponentInChildren<TMP_Text>().text = "To earn a cookie you need to punch it, \n To punch an object you need a fist \n To make a fist press 'Grip' and 'Trigger' button";
-                    yield return new WaitForSeconds(10f);
+                    //rigCanva.GetComponentInChildren<TMP_Text>().text = "To earn a cookie you need to punch it, \n To punch an object you need a fist \n To make a fist press 'Grip' and 'Trigger' button";                   
+                    //yield return new WaitForSeconds(10f);
+                    StartCoroutine(InstructionTexts(rigCanva.GetComponentInChildren<TMP_Text>()));
+                    while(areInfoTextRunning)
+                    {
+                        yield return null;
+                    }
                     sequence = startSequence.enableMarker;
                     break;
                 case startSequence.enableMarker:
@@ -92,5 +101,18 @@ public class TutorialSequence : MonoBehaviour
             }
             yield return null;
         }
+    }
+    IEnumerator InstructionTexts(TMP_Text text)
+    {
+        areInfoTextRunning = true;
+        locomotion.enabled = false;
+        text.text = "To earn a cookie you need to punch it, \n To punch an object you need a fist \n To make a fist press 'Grip' and 'Trigger' button";
+        yield return new WaitForSeconds(10f);
+        text.text = "To move around use left hands joystick";
+        locomotion.enabled = true;
+        yield return new WaitForSeconds(5f);
+        text.text = "";
+        areInfoTextRunning = false;
+
     }
 }
